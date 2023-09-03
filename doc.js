620d9487c307d770a98ce167107d0e373d76b446
Shutpix <vishalmishra1249@gmail.com>
2023-09-03 21:06:34 +0530

    First Commit

---
+++ doc.js
@@ -0,0 +1,16 @@
+var items= document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hellow 2';
+item[1].style.fontWeight = 'bold';
+item[1].style.backgroundColor = 'yellow';
+
+
+for(var i=0;i<items.length;i++)
+{
+    item[i].style.backgroundColor = '#f4f4f4';
+
+
+}
+
+
